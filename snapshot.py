d1623ef25a2bd35fa92fc62dd3d6b25ada3d98a3
Jay Hood <hood.jay@gmail.com>
2018-10-07 11:59:32 -0400

    added relevant comments and changed outdated file paths for test files

--- snapshot.py
+++ snapshot.py
@@ -1,11 +1,16 @@
 import requests
 import string
 
+# Parameterized search string that should allow us to fetch a total list of all candidates
 search_string = 'http://media.ethics.ga.gov/search/Campaign/Campaign_Namesearchresults.aspx?CommitteeName=&LastName={}&FirstName=&Method=0'
+
+# Lowercase a - z iterator used for formatting urls and file names
 for char in string.ascii_lowercase:
     current_string = search_string.format(char)
     file_name = 'candidate_{}'.format(char)
     page = requests.get(search_string)
+
+    # Save results to local files for easier testing
     with open('/home/jay/projects/python_projects/campaign_finance_scraper/samples/{}'.format(file_name), 'w') as f:
         f.write(page.text)
         f.close()
--- tests/xpath_test.py
+++ tests/xpath_test.py
@@ -1,9 +1,14 @@
 from lxml import html 
 
-with open('/home/jay/projects/python_projects/campaign_finance_scraper/samples/test_profile.html') as f:
+# Load sample file for parsing
+with open('/home/jay/projects/python_projects/campaign_finance_scraper/samples/candidate_a.html') as f:
     page = f.read()
+    # Treeify the plaintext html
     tree = html.fromstring(page)
+    # Xpath parses the DOM and creates a list of objects matching the designated criteria
+    # In the below example the leading './/' signifies to search amongst all elements that are descendants of the root node
     res = tree.xpath(".//a[@class='lblentrylink']")
     for item in res:
+        # .get(attribute) methods are used to extract data from HTMLElements
         print(item.get('id'))
     f.close()
